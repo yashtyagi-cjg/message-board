b3cf8fcc212295a6b4a69228949d1a62bc248cd5
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-16 06:10:51 +0530

    Initial Configuration and Db Setup

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var MongoStore = require('connect-mongo');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +17,24 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+//SESSION CONFIRGURATION
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.MONGODB_URI,
+  dbName: process.env.MONGODB_DBNAME,
+  collection: 'session'
+})
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  store: sessionStore
+}))
+
+app.use(passport.session())
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,5 +60,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+/* Connecting to Mongo DB*/
+async function main(){
+  mongoose.connect(process.env.MONGODB_URI,{
+    dbName: process.env.MONGODB_DBNAME
+  })
+  console.log(`Connected to Mongo DB @ ${process.env.MONGODB_DBNAME}`)
+}
+
+main.catch((err)=>console.log(err));
 
 module.exports = app;
