3e81562cccef63fd0cf218d4d4afadbc49752240
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-16 06:44:33 +0530

    User Data Model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    username: {type: String},
+    hash: {type: String},
+    salt: {type: String},
+    isAdmin: {type: Boolean},
+    isMember: {type: Boolean}
+})
+
+module.exports = Schema.model('User', User);
