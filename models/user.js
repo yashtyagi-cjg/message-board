81e433b4bf7efc2444917b7c9dca5a55bfcee56e
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-22 01:07:03 +0530

    Modified fields in User ODM/Collection

--- models/user.js
+++ models/user.js
@@ -3,10 +3,16 @@ const Schema = mongoose.Schema;
 
 const User = new Schema({
     username: {type: String},
+    firstname: {type: String},
+    lastname: {type: String},
     hash: {type: String},
     salt: {type: String},
     isAdmin: {type: Boolean},
     isMember: {type: Boolean}
+})
+
+User.virtual('name').get(function(){
+    return `${this.firstname} ${this.lastname}`
 })
 
 module.exports = mongoose.model('User', User);
