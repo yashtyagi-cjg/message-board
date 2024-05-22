c8971855ba0bdc5262c9c7b96557075269b0eb8a
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-22 21:45:21 +0530

    Added catch blocks

--- config/passport.js
+++ config/passport.js
@@ -7,16 +7,20 @@ const validatePassword = require('./../utils/passwordUtils').validatePassword;
 passport.use(new LocalStratergy((username, password, cb)=>{
     User.findOne({username: username}).exec().then((user)=>{
         if(!user){
+            console.log(username, password, '0')
             return cb(null, false, {message: "Incorrect Username or Password"});
         }
-
+        console.log(username, password, '1')
         const isValid = validatePassword(password, user.hash, user.salt);
 
         if(isValid){
-            return cb(null, true);
+            return cb(null, user);
         }else{
             return cb(null, false, {message: "Incorrect Username or Password."})
         }
+    }).catch((err)=>{
+        console.log(`Encountered error while processing ${err}`)
+        return cb(err);
     })
 
 }))
@@ -30,7 +34,8 @@ passport.deserializeUser(async (userId, cb)=>{
     try{
         const user = await User.findById(userId).exec();
         return cb(null, user);
-    }catch(err){
-        return cb(err)
+    }catch (err) {
+        console.error(`Error occurred during deserialization: ${err}`);
+        return cb(err);
     }
 })
