b0a29ad980b4bad75a366eface24e72d22ed6af3
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-22 21:50:51 +0530

    Route Changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const Homepage = require('./../controllers/homepage');
+const Post = require('./../controllers/post');
+const Auth = require('./../controllers/authentication')
+const passport = require('passport')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', Homepage.get_homepage);
+
+
+router.get('/new', Post.get_newPost);
+
+
+router.post('/new', Post.post_newPost);
+
+router.get('/login', Auth.get_login);
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+router.get('/logout', Auth.get_logout);
+
+router.get('/signup', Auth.get_signup);
+
+router.post('/signup', Auth.post_signup);
+
 
 module.exports = router;
