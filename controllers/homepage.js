782339b8a79d7e818109f5220efe6cce91eb9688
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-23 00:32:57 +0530

    Messages to be displayed on homepage

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,15 +1,17 @@
 const {body, resultValidation} = require('express-validator')
 const asyncHandler = require('express-async-handler')
-
+const Post = require('./../models/post')
 
 exports.get_homepage = asyncHandler(
     async(req, res, next)=>{
+        const messages = await Post.find({}).sort({time: 1}).exec();
         console.log(req.user)
         res.render("homepage",
             {
                 title: "Messages",
-                messages: undefined,
+                posts: (messages.length > 0?messages:undefined),
                 user: req.user,
+                isAdmin: (req.user===undefined?undefined:req.user.isAdmin)
             }
         )
     }
